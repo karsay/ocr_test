bdcbf4b01ecab3a893aae4da9191f2f1da7ff657
karsay <ryu47seinou@gmail.com>
2022-07-13 13:20:36 +0900

    firstcomit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <input type="file" ref="preview" @change="uploadFile" />
+  <button v-on:click="hoge">upload</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  contentBuffer:{
+    aaa:"hoge"
+  },
+  methods: {
+    async hoge() {
+      const COMPUTER_VISION_API_ENDPOINT_URL =
+        "https://japaneast.api.cognitive.microsoft.com/vision/v3.0/ocr?language=unk&detectOrientation=true";
+
+      const config = {
+        url: COMPUTER_VISION_API_ENDPOINT_URL,
+        method: "post",
+        headers: {
+          "Content-type": "application/octet-stream",
+          "Ocp-Apim-Subscription-Key": "82b9da35112c4f89941a116f68b14fd1",
+        },
+        data: this.$refs.preview.files[0],
+      };
+
+      const responseAzure = await axios.request(config);
+      var result = responseAzure.data
+      for (let i=0; i<result.regions.length; i++){
+            const resultRegionsLines = result.regions[i].lines;
+            for (let j=0; j<resultRegionsLines.length; j++) {
+                const resultRegionsLinesWords = resultRegionsLines[j].words;
+                let currentLineTexts = "";
+                for (let k=0; k<resultRegionsLinesWords.length; k++) {
+                    currentLineTexts += resultRegionsLinesWords[k].text;
+                }
+                console.log(currentLineTexts);
+            }
+      }
+      console.log("post OK");
+    },
+    uploadFile() {
+      console.log(this.$refs.preview.files[0]);
+    },
+  },
+};
 </script>
 
 <style>
