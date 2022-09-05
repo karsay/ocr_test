72daa1982601b7c94c3856be72c1d7ef6f831565
karsay <ryu47seinou@gmail.com>
2022-09-05 13:12:24 +0900

    a

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,22 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "App",
-  contentBuffer:{
-    aaa:"hoge"
-  },
+  data() {},
   methods: {
     async hoge() {
+      var ocrText = "";
+      const sleep = (waitTime) =>
+        new Promise((resolve) => setTimeout(resolve, waitTime));
       const COMPUTER_VISION_API_ENDPOINT_URL =
-        "https://japaneast.api.cognitive.microsoft.com/vision/v3.0/ocr?language=unk&detectOrientation=true";
+        "https://japaneast.api.cognitive.microsoft.com/vision/v3.2/read/analyze";
+      const TEXT_ANALYTICS_API_ENDPOINT_URL =
+        "https://omoikane-analytics.cognitiveservices.azure.com/language/:analyze-text?api-version=2022-05-01";
 
-      const config = {
+      var config = {
         url: COMPUTER_VISION_API_ENDPOINT_URL,
         method: "post",
         headers: {
@@ -27,20 +30,59 @@ export default {
         data: this.$refs.preview.files[0],
       };
 
-      const responseAzure = await axios.request(config);
-      var result = responseAzure.data
-      for (let i=0; i<result.regions.length; i++){
-            const resultRegionsLines = result.regions[i].lines;
-            for (let j=0; j<resultRegionsLines.length; j++) {
-                const resultRegionsLinesWords = resultRegionsLines[j].words;
-                let currentLineTexts = "";
-                for (let k=0; k<resultRegionsLinesWords.length; k++) {
-                    currentLineTexts += resultRegionsLinesWords[k].text;
-                }
-                console.log(currentLineTexts);
+      // ocr処理
+      await axios.request(config).then(async function (response) {
+        config = {
+          url: response.headers["operation-location"],
+          method: "get",
+          headers: {
+            "Ocp-Apim-Subscription-Key": "82b9da35112c4f89941a116f68b14fd1",
+          },
+        };
+        //2秒待つ
+        await sleep(2000);
+
+        // ocrデータの取得
+        axios.request(config).then(async function (response) {
+          var result = response.data.analyzeResult;
+          // データの整形
+          for (let i = 0; i < result.readResults.length; i++) {
+            const resultRegionsLines = result.readResults[i].lines;
+            for (let j = 0; j < resultRegionsLines.length; j++) {
+              const resultRegionsLinesWords = resultRegionsLines[j].words;
+              var currentLineTexts = "";
+              for (let k = 0; k < resultRegionsLinesWords.length; k++) {
+                currentLineTexts += resultRegionsLinesWords[k].text;
+                ocrText += resultRegionsLinesWords[k].text;
+              }
+              console.log(currentLineTexts);
             }
-      }
-      console.log("post OK");
+          }
+          // キーフレーズ取得
+          config = {
+            url: TEXT_ANALYTICS_API_ENDPOINT_URL,
+            method: "post",
+            headers: {
+              "Content-type": "application/json",
+              "Ocp-Apim-Subscription-Key": "fdd507dc09a44bf6a50dc6b4f60982dd",
+            },
+            data: {
+              kind: "KeyPhraseExtraction",
+              analysisInput: {
+                documents: [
+                  {
+                    id: "1",
+                    text: ocrText,
+                    language: "ja",
+                  },
+                ],
+              },
+            },
+          };
+          response = await axios.request(config);
+          console.log(response.data.results.documents[0].keyPhrases);
+        });
+      });
     },
     uploadFile() {
       console.log(this.$refs.preview.files[0]);
